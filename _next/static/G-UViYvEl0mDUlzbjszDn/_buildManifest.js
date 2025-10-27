e23cf8e5c3ebada2be40e2128f16bbf05e2a17de
Nikita Lipatov <6stringsohei@gmail.com>
2025-10-27 15:42:30 +0100

    Updates

--- _next/static/G-UViYvEl0mDUlzbjszDn/_buildManifest.js
+++ _next/static/G-UViYvEl0mDUlzbjszDn/_buildManifest.js
@@ -1,12 +1,7 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [
-      {
-        "source": "/ethacore.com//_next/:path+",
-        "destination": "/ethacore.com/_next/:path+"
-      }
-    ],
+    "beforeFiles": [],
     "fallback": []
   },
   "sortedPages": [
